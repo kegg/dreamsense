7b19a41abe15f97ef5a5f251a6689faa9fa5ebc2
Kyle Eggleston <kyle@dreamsense.com>
2020-02-19 16:16:25 -0700

    Corrected drop caps

--- 2020/0219.php
+++ 2020/0219.php
@@ -3,10 +3,20 @@
   include("../header.php");
 ?>
 
-<p>So much noise going on in the house. Not sure what to do
+<p><span class="drop">S</span>o much noise going on in the 
+house. Not sure what to do
 about all of it. Just lots of noise. I'm not a fan of noise,
 not in the least. It's a bit overwhelming. I'd rather not
 have to deal with any of it, but well you know it happens.</p>
+
+<p>I'd rather not deal with the noise. I mean, I've already got noise going on 
+in my own head, why would I want the extra noise to be there? Doesn't make sense 
+right? No, I didn't think so. Well, here's to the noise that I'm trying to deal 
+with.</p>
+
+<p>Noise comes in so many levels and functions. There's visual noise, auditory 
+noise... probably others I can't even think of at the moment. But if it can 
+affect your senses, it can be noise.</p>
 
 <?php
   include("../footer.php");
--- dream.php
+++ dream.php
@@ -3,9 +3,14 @@
   include("header.php");
 ?>
 
-<p>Ah dreams, here we are with the dreams. Dreams come and go long before we
+<p><span class="drop">A</span>h dreams, here we are with the dreams. 
+  Dreams come and go long before we
   are able to understand and comprehend them. They are sometimes the inner
   most desires of our heart.</p>
+
+<p>A dream can mean many things to different people. It doesn't always have to 
+mean the same thing to you or them or anyone else. YOU are your own dreamer. 
+Make sure that you determine what your dreams mean.</p>
 
 <?php
   include("footer.php");
