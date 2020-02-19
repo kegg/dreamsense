ebe7cc0912dfd5ced621f9a25b41dfa6eece65f9
Kyle Eggleston <kyle@dreamsense.com>
2020-02-19 13:52:20 -0700

    Added slogan

--- footer.php
+++ footer.php
@@ -5,7 +5,8 @@
   echo '<a href="mailto:kyle@dreamsense.com">kyle@dreamsense.com</a><br/>';
   echo 'Copyright &copy; <a href="2003.php">2003</a> - ' . date('Y');
   echo '</address>';
-  echo '<p style="text-align:center""><a href="/index.php"><img border="0" src="/ds.png"/></a></p>';
+  echo '<p style="text-align:center""><a href="/index.php"><img border="0" src="/ds.png"/></a><br/>';
+  echo '<img border="0" src="/ds2.png"/></p>';
   echo '</div>';
   echo "</body>";
   echo "</html>";
