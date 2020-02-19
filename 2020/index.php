b93ffe232a3110bdae058584283dfb4ade8e150e
Kyle Eggleston <kyle@dreamsense.com>
2020-02-19 14:07:18 -0700

    Added 2020

---
+++ 2020/index.php
@@ -0,0 +1,10 @@
+<?php
+  $title = "2020";
+  include("../header.php");
+?>
+
+<p>A brand new year and here we go.</p>
+
+<?php
+  include("../footer.php");
+?>
--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <?php
   echo '<hr/>';
   echo '<address>';
-  echo '<a href="about.php">Kyle Eggleston</a><br/>';
+  echo '<a href="/about.php">Kyle Eggleston</a><br/>';
   echo '<a href="mailto:kyle@dreamsense.com">kyle@dreamsense.com</a><br/>';
-  echo 'Copyright &copy; <a href="2003.php">2003</a> - ' . date('Y');
+  echo 'Copyright &copy; <a href="/2003.php">2003</a> - ';
+  echo '<a href="/' . date('Y') . '/index.php">' . date('Y') . '</a>';
   echo '</address>';
   echo '<p style="text-align:center""><a href="/index.php"><img border="0" src="/ds.png"/></a><br/>';
   echo '<img border="0" src="/ds2.png"/></p>';
