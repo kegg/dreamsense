8429c55863e9d277323dabc9bf61091bb34630b8
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-20 12:08:49 -0700

    Added drop class.

--- 2003.php
+++ 2003.php
@@ -3,8 +3,8 @@
   include("header.php");
 ?>
 
-<p>Ah 2003, when life felt much different and weird...or was it simple? I dont' 
-know. All I know is that's when I purchased dreamsense.com. Talk about some good 
+<p><span class="drop">A</span>h 2003, when life felt much different and weird...or was it simple? I dont' 
+know. All I know is that's when I purchased dreamsense.com. Talk about some good
 times for sure.</p>
 
 <p style="text-align:center"><img src="/img/heart.png"></p>
