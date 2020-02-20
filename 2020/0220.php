4eb80c915782c781cf00c883633e6a6a95d25c0f
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-20 07:35:51 -0700

    Added Feb 20th

---
+++ 2020/0220.php
@@ -0,0 +1,14 @@
+<?php
+  $title = "Feb 20, 2020";
+  include("../header.php");
+?>
+
+<p><span class="drop">T</span>oday is just another day
+well, some would have you believe that. But if you allow
+yourself to grasp hold of today and see what happens,
+you are able to take a better hold of today than you
+originally thought.</p>  
+
+<?php
+  include("../footer.php");
+?>
--- 2020/index.php
+++ 2020/index.php
@@ -5,7 +5,11 @@
 
 <p>A brand new year and here we go.</p>
 
-<p><a href="0219.php">Feb 19, 2020</a></p>
+<p>
+<a href="0219.php">Feb 19, 2020</a><br/>
+<a href="0220.php">Feb 20, 2020</a>
+
+</p>
 
 <?php
   include("../footer.php");
