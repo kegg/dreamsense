3149ffb8eeaf0c3efbe02a5189d76d49e48d4a78
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-20 09:29:33 -0700

    Added links to mormon and code subdomains

--- footer.php
+++ footer.php
@@ -5,6 +5,8 @@
   echo '<a href="mailto:kyle@dreamsense.com">kyle@dreamsense.com</a><br/>';
   echo 'Copyright &copy; <a href="/2003.php">2003</a> - ';
   echo '<a href="/' . date('Y') . '/index.php">' . date('Y') . '</a>';
+  echo ' <a href="https://mormon.dreamsense.com">Mormon</a> ';
+  echo ' <a href="https://code.dreamsense.com">Code</a> ';
   echo '</address>';
   echo '<p style="text-align:center""><a href="/index.php"><img border="0" src="/ds.png"/></a><br/>';
   echo '<a href="/dream.php"><img border="0" src="/ds2.png"/></a></p>';
