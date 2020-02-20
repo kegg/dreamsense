886f2630cda39dbcc3e6cbe6de7183beccb109e7
Kyle Alan Eggleston <kylealaneggleston@Kyles-Mac-mini.local>
2020-02-20 12:06:37 -0700

    Added content.

--- about.php
+++ about.php
@@ -5,8 +5,10 @@
   include("header.php");
 ?>
 
-<p><span class="drop">I'm</span> just a guy who wants to figure out what's going on in this life. It 
+<p><span class="drop">I'</span>m just a guy who wants to figure out what's going on in this life. It
 doesn't always mean something good will happen, but here we are nonetheless.</p>
+
+<p>Life can be weird, that much is certain. That's how everything works around here.</p>
 
 <p style="text-align:center">
   <img src="/img/kyle.jpg"/><br/>
