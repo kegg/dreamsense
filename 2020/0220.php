433b21588dd26821b9d0a204d2b312787b423892
Kyle Eggleston <kyle@dreamsense.com>
2020-02-20 18:50:15 -0700

    Update 0220.php

--- 2020/0220.php
+++ 2020/0220.php
@@ -1,13 +1,29 @@
 <?php
   $title = "Feb 20, 2020";
-  include("../header.php");
+  $prev = "0219.php";
+  include("../header.php"); 
 ?>
+
+<p>08:30</p>
 
 <p><span class="drop">T</span>oday is just another day
 well, some would have you believe that. But if you allow
 yourself to grasp hold of today and see what happens,
 you are able to take a better hold of today than you
-originally thought.</p>  
+originally thought.</p>
+
+<p>13:00</p>
+
+<p>Uh, yeah all of that. Not sure what I was even saying.
+  trying to figure that out right now.</p>
+
+<p>I think I was trying to say that if you grab hold of
+  what you can accomblish today, not worrying about
+  tomorrow until it gets here (yet still being prepared
+  for the tomorrow that might come) you are able to
+  better handle whatever comes your way today.</p>
+
+<p>Yep! Exactly that!</p>
 
 <?php
   include("../footer.php");
